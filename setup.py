bc5ff3037b6a2af0232324f947d64a03c3ed9484
Bian Li <bian.li@yale.edu>
2020-02-07 11:43:40 -0500

    Added setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from distutils.core import setup
+
+setup(
+    name='ThermoNet',
+    version='1.0',
+    author='Bian Li',
+    author_email='bian.li@yale.edu',
+    packages=['ThermoNet', 'utils'],
+)
